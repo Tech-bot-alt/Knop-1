a4be7710cc806a592fc0106821a5d05bdb968103
Tech-bot-alt <paul.tvog@hotmail.com>
2025-06-13 09:58:25 +0200

    Update signaal.php

--- signaal.php
+++ signaal.php
@@ -1,6 +1,4 @@
 <?php
-session_start();
-$_SESSION['knop1'] = true;
-file_put_contents('signalen.log', "Knop 1 werd geactiveerd door sessie op ".date('c')."\n", FILE_APPEND);
-echo json_encode(['success'=>true]);
+header('Content-Type: application/json');
+echo json_encode(['status' => 'ok']);
 ?>
